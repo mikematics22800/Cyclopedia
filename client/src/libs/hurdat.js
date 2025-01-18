aaaeede10733db2e57ec2df687ceeeb30a4bce4f
Michael Medina <mikematics22800@gmail.com>
2025-01-18 13:42:27 -0500

    updated api url

--- client/src/libs/hurdat.js
+++ client/src/libs/hurdat.js
@@ -6,7 +6,7 @@ const options = {
 }
 
 export const getHurdat = async (basin, year) => {
-  const response = await fetch(`https://atlantic-hurricane-database.onrender.com/${basin}/${year}`, options)
+  const response = await fetch(`https://cyclopedia.com/${basin}/${year}`, options)
   const data = await response.json();
   return data;
 }
