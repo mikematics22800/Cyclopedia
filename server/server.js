23dad4b30b0a450fd3bda3349e7169c23bbf9639
Michael Medina <mikematics22800@gmail.com>
2025-01-18 13:38:05 -0500

    added server directory

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors'; 
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://mikematics22800.github.io', 'https://storm-cyclopedia.com'],
+  methods: ['GET', 'POST', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); 
+app.use(express.static('public')); 
+
+app.get('/:basin/:year', (req, res) => {
+  const year = req.params.year;
+  const basin = req.params.basin;
+  const filePath = path.join(__dirname, `./${basin}/${year}.json`);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(404).send('File not found'); 
+    }
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (parseError) {
+      console.error(parseError);
+      res.status(500).send('Error parsing JSON data');
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
