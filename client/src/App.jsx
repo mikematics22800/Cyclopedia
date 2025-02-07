0d007d24551f38a80143cf42f85ea7bca2b273d4
Michael Medina <mikematics22800@gmail.com>
2025-02-07 15:29:11 -0500

    cyclopedia is now installable!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,18 @@ function App() {
   const [windField, setWindField] = useState(false)
 
   useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      });
+    }
+  }, [])
+
+  useEffect(() => {
     if (year < 1949 && basin === 'pac') setYear(1949)
     const cache = localStorage.getItem(`cyclopedia-${basin}-${year}`)
     if (cache) {
